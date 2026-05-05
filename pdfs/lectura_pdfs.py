fdd3b68366cde7e07092ca07077a61b7920b0c56
Aitor Donado <aitordonado@gmail.com>
2026-05-05 10:21:04 +0200

    Inicio pdfs

--- pdfs/lectura_pdfs.py
+++ pdfs/lectura_pdfs.py
@@ -1,7 +1,13 @@
 from PyPDF2 import PdfReader
 
-reader = PdfReader("pdfs/339741.pdf")
+reader1 = PdfReader("pdfs/339741.pdf")
+reader2 = PdfReader("pdfs/FDS 2023 - SALFUMAN - AGUA FUERTE.pdf")
+reader3 = PdfReader("pdfs/SDB-9277-ES-ES.pdf")
+"""
 number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
+text = ""
+for page in reader.pages:
+    text += page.extract_text()
 print(text)
+"""
+print(reader2.pages[0].extract_text())
